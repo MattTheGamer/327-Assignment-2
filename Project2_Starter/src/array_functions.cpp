46c4c20b3eb24eb0689f70722699591dc18e31a0
matt <matt@matt-VirtualBox>
2018-02-02 18:33:36 -0500

    I have no idea what I'm doing. I added all functions and a struct

--- Project2_Starter/src/array_functions.cpp
+++ Project2_Starter/src/array_functions.cpp
@@ -1,21 +1,112 @@
 /*
  * functionstocomplete.cpp
  *
- *  Created on: Sep 10, 2017
- *      Author: keith
+ *  Created on: Feb 2, 2018
+ *      Author: Matt Passarelli
  */
 
 //============================================================================
 //	TODO add necessary includes here
+#include "array_functions.h"
+#include <iostream>
+#include <fstream>
 //============================================================================
 
 //============================================================================
 //	stuff you will need
+using namespace std;
 //============================================================================
 //TODO define a structure to track words and number of times they occur
+struct entry {
+	string word;
+	int occurences;
+};
 
 //TODO add a global array of entry structs (global to this file)
+//TODO need new number. Not 10 entry list[10];
 
 //TODO add variable to keep track of next available slot in array
 
-//TODO define all functions in header file
+
+//zero out array that tracks words and their occurrences
+void clearArray() {
+
+}
+
+//how many unique words are in array
+int getArraySize() {
+
+	return 0;
+}
+
+//get data at a particular location
+string getArrayWordAt(int i) {
+
+	return "";
+}
+
+int getArrayWord_NumbOccur_At(int i) {
+
+	return 0;
+}
+
+/*loop through whole file, one line at a time
+ * call processLine on each line
+ * returns false: myfstream is not open
+ *         true: otherwise*/
+bool processFile(fstream &myfstream) {
+
+	return false;
+}
+
+/*take 1 line and extract all the tokens from it
+ feed each token to processToken for recording*/
+void processLine(string &myString) {
+
+}
+
+/*Keep track of how many times each token seen*/
+void processToken(std::string &token) {
+
+}
+
+/*if you are debugging the file must be in the project parent directory
+ in this case Project2 with the .project and .cProject files*/
+bool openFile(std::fstream& myfile, const std::string& myFileName,
+		std::ios_base::openmode mode) {
+
+	ifstream myInputFile;
+	myInputFile.open(myFileName.c_str(), ios::in);
+
+	if (myInputFile.is_open()) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+/*iff myfile is open then close it*/
+void closeFile(std::fstream& myfile) {
+
+}
+
+/* serializes all content in myEntryArray to file outputfilename
+ * returns  FAIL_FILE_DID_NOT_OPEN if cannot open outputfilename
+ * 			FAIL_NO_ARRAY_DATA if there are 0 entries in myEntryArray
+ * 			SUCCESS if all data is written and outputfilename closes OK
+ * */
+int writeArraytoFile(const std::string &outputfilename) {
+
+	return 0;
+}
+
+/*
+ * Sort myEntryArray based on so enum value.
+ * You must provide a solution that handles alphabetic sorting (A-Z)
+ * The presence of the enum implies a switch statement based on its value
+ * You are provided with a myentry compare function in the cpp file
+ */
+void sortArray(constants::sortOrder so) {
+
+}
+
